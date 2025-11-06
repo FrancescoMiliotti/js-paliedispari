14ae597a14d4169837efda583c750bc678760ee3
francesco.miliotti <francesco.miliotti@email.com>
2025-11-06 13:53:41 +0100

    creo una funzione per capire se è palindroma

--- Palindroma/Palindroma.js
+++ Palindroma/Palindroma.js
@@ -5,7 +5,7 @@ const inputUtente = prompt("Scrivi una parola");
 
 // salvo la parola in una variabile e stampo in console
 
-console.log("Hai scelto la parola :" , inputUtente);
+console.log("Hai scelto la parola :", inputUtente);
 
 
 //scompongo la parola per ogni lettera e inserisco le lettere in un array
@@ -14,4 +14,33 @@ let parolaUtente = []
 for (let i = 0; i < inputUtente.length; i++) {
     parolaUtente.push(inputUtente.charAt(i));
 };
-console.log("Array parola Utente" ,parolaUtente)
+console.log("Array parola Utente", parolaUtente)
+
+// inverto l'ordine dell'array e lo converto in stringa
+
+const reversedParolaUtente = parolaUtente.reverse();
+let parolaToString = reversedParolaUtente.join("");
+console.log("Parola Invertita :", parolaToString)
+
+//Creare una funzione per capire se la parola inserita è palindroma.
+
+
+function palindroma(stringa) {
+    
+    if(stringa == inputUtente) {
+        alert("la parola scelta è palindroma!")
+        return "palindroma";
+        
+    }
+    else{
+        alert("la parola scelta NON è palindroma!")
+        return "notpalindroma";
+    }
+   
+
+}
+
+let ispalindroma = palindroma (parolaToString);
+console.log("la parola scelta è :", ispalindroma);
+
+
